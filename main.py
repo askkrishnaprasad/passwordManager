700a87273b30784633007b2c8253e5e7218fb026
askkrishnaprasad <askkrishnaprasad@gmail.com>
2023-01-02 16:26:53 -0500

    Added exception handling and searching functionality to the program

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -32,22 +33,52 @@ def password_gen():
 
     # Add password to clipboard
     pyperclip.copy(password_generated)
-    
 
     print(f"Your password is: {password_generated}")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def find_password():
+    if website.get() == "":
+        messagebox.showerror(title="Error", message="Make sure to enter value of website before Searching.!")
+    else:
+        try:
+            with open("data.json", mode="r") as output_file:
+                data = json.load(output_file)
+                email_value = data[website.get()]["email"]
+                pass_value = data[website.get()]["password"]
+                messagebox.showinfo(title="Info",
+                                    message=f"Email: {email_value}\n Password: {pass_value}")
+        except FileNotFoundError:
+            messagebox.showerror(title="Error", message="Details are not yet provided.!")
+        except KeyError:
+            messagebox.showerror(title="Error", message="Details are not yet provided.!")
+
+
 def save():
+    new_data = {
+        website.get(): {
+            "email": username.get(),
+            "password": password.get()
+        }}
     if len(website.get()) == 0 or len(password.get()) == 0:
         messagebox.showinfo(title="Oops", message="Make sure to enter values before saving.!")
     else:
-        is_ok = messagebox.askokcancel(title=website.get(),
-                                       message=f"These are the details entered:\nEmail: {username.get()}\n Password: {password.get()}\nIs it ok to save")
-        if is_ok:
-            with open("data.txt", mode="a") as output_file:
-                new_content = website.get() + " | " + username.get() + " | " + password.get() + "\n"
-                output_file.write(new_content)
+        try:
+            with open("data.json", mode="r") as output_file:
+                # Read old data
+                data = json.load(output_file)
+                # Update the old data with the new one
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as output_file:
+                # Add it to json
+                json.dump(new_data, output_file, indent=4)
+        else:
+            with open("data.json", mode="w") as output_file:
+                # Add it to json
+                json.dump(data, output_file, indent=4)
+        finally:
             website.delete(0, END)
             password.delete(0, END)
 
@@ -64,14 +95,14 @@ canvas.grid(column=2, row=1)
 
 label_site = Label(text="Website:")
 label_site.grid(column=1, row=2)
-website = Entry(width=35)
-website.grid(column=2, row=2, columnspan=2)
+website = Entry(width=21)
+website.grid(column=2, row=2)
 # when the program starts focus method will make the default entry option on email
 website.focus()
 
 label_uname = Label(text="Email/Username:")
 label_uname.grid(column=1, row=3)
-username = Entry(width=35)
+username = Entry(width=38)
 username.grid(column=2, row=3, columnspan=2)
 username.insert(0, "example_email@gmail.com")
 
@@ -82,6 +113,9 @@ password.grid(column=2, row=4)
 
 genpass_button = Button(text="Generate Password", highlightthickness=0, command=password_gen)
 genpass_button.grid(column=3, row=4)
+
+search_button = Button(text="Search", highlightthickness=0, command=find_password, width=13)
+search_button.grid(column=3, row=2)
 
 addpass_button = Button(text="Add", highlightthickness=0, width=34, command=save)
 addpass_button.grid(column=2, row=5, columnspan=2)
